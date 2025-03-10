18e119dc16cfd9b7f8ea23e73a2e3d93320381de
Karthish716 <karthish2001@gmail.com>
2025-03-10 15:36:35 +0530

    feat: useRef Hook

--- CodeOnChange/src/Counter.jsx
+++ CodeOnChange/src/Counter.jsx
@@ -1,23 +1,31 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState, useRef } from "react";
 import { dataConstext } from "./Home";
 
 function Counter (){
     const [count, setCount] = useState(0);
 
+    let refCount = useRef(0);
+
     const data = useContext(dataConstext);
 
     function inc(){
-        setCount(preCount => preCount+1);
+       // setCount(preCount => preCount+1);
         //setCount(preCount => preCount+1);
+        setCount(count+1);
+        refCount.current ++;
+        console.log('count:'+count);
+        console.log('refcount:'+refCount.current);
     }
     useEffect(()=>{
-        console.log(count);
-    },[count]);
+        //console.log(count);
+        //console.log(refCount);
+    });
     return (
         <>
         <h2>{data}</h2>
-        <h2 className="m-5">{count}</h2>
-        <button className='mx-5' onClick={inc}>UP</button>
+        <h2 className="m-5">{refCount.current}</h2>
+        <button className='mx-5 btn btn-primary' onClick={inc}>UP</button>
+        <button className='btn btn-secondary' onClick={()=>{setCount(refCount.current)}}>UPdate</button>
         </>
     );
 }
