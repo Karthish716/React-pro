197ca253ddc289c238ba315517d8150f6ab2683f
Karthish716 <karthish2001@gmail.com>
2025-03-10 12:05:16 +0530

    feat: React Router and Link Compnent

---
+++ CodeOnChange/src/NotFount.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+function NotFound (){
+    return(
+        <>
+        <h2>Error 404 Page not Found</h2>
+        <a className="btn btn-primary" href="/">Home</a>
+        <Link className="btn btn-danger" to="/">Home</Link>
+        </>
+    );
+}
+
+export default NotFound;
